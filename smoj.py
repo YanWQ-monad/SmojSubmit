2a228c4b67afcf9524376491b052524e4d044539
YanWQ-monad <YanWQmonad@gmail.com>
2018-05-18 07:49:21 +0800

    Change to ApplicationCommand

--- smoj.py
+++ smoj.py
@@ -5,6 +5,7 @@ import sublime
 import re
 
 from .SMOJ import login, result, post, config
+from .plug import timer
 from . import common
 from . import log
 
@@ -13,25 +14,31 @@ _fre_re = re.compile(r'freopen\("([^.])+\.(in|out)"( ?), "(r|w)", std(in|out)( ?
 _cm1_re = re.compile(r'/\*(\s*)((freopen(.*,.*,.*)\s*){1,2})\s*\*/')
 _cm2_re = re.compile(r'(\s*)//(\s*)(freopen\("([^.])+\.(in|out)"( ?), "(r|w)", std(in|out)( ?)\);)')
 
-class SmojSubmitCommand(sublime_plugin.TextCommand):
-    def __init__(self, view):
+settings = None
+config_callback = None
+
+class SmojSubmitCommand(sublime_plugin.ApplicationCommand):
+    def __init__(self):
+        sublime_plugin.ApplicationCommand.__init__(self)
+        log.info('Delay to load')
         self.Login = False
-        if not self.legalFileName(view.file_name()):
-            return None
+        timer.Timer(3, self.configs)
+
+    def configs(self):
         setting = sublime.load_settings('SmojSubmit.sublime-settings')
         if not setting.get('smoj').get('enable'):
             return None
-        log.info('Load SMOJ plugin')
         setting.add_on_change(common.PLUGIN_NAME, self.reload_settings)
         setting  = setting.get('smoj')
         username = setting.get('username')
         password = setting.get('password')
         self.login(username, password)
-        sublime_plugin.TextCommand.__init__(self, view)
+        log.info('SMOJ plugin loaded')
+        self.setting = setting
 
     def legalFileName(self, name):
         if name is None:
-            return True
+            return False
         if len(name) < 4:
             return False
         if name[-4:] in ('.cpp'):
@@ -52,6 +59,7 @@ class SmojSubmitCommand(sublime_plugin.TextCommand):
         username = setting.get('username')
         password = setting.get('password')
         self.relogin(username, password)
+        self.setting = setting
 
     def setOpener(self, opener):
         self.opener = opener
@@ -59,8 +67,8 @@ class SmojSubmitCommand(sublime_plugin.TextCommand):
     def setLoginFlag(self, flag):
         self.Login = flag
 
-    def post(self, cpp, problem):
-        result_thread = result.ResultThreading(self.opener, self.view)
+    def post(self, cpp, problem, view):
+        result_thread = result.ResultThreading(self.opener, view)
     #    result_thread.start()
         thread = post.PostThreading(self.opener, cpp, problem, result_thread.start)
         thread.start()
@@ -74,22 +82,21 @@ class SmojSubmitCommand(sublime_plugin.TextCommand):
         thread = login.LoginThreading(username, password, self.setOpener, self.setLoginFlag, False)
         thread.start()
 
-    def getProblemNum(self):
-        sublime.status_message('Search problem number')
-        chunk = self.view.find_all(r'// ?(\d{4})\.cpp', 0)
+    def getProblemNum(self, view):
+        chunk = view.find_all(r'// ?(\d{4})\.cpp', 0)
         if len(chunk) < 1:
             log.warning           ('Not found problem number')
             sublime.status_message('Not found problem number')
             sublime. error_message('Not found problem number')
             return None
         chunk = chunk[0]
-        cpp_name = self.view.substr(sublime.Region(chunk.a, chunk.b))
+        cpp_name = view.substr(sublime.Region(chunk.a, chunk.b))
         m = _cpp_re.search(cpp_name)
         cpp_num  = m.group(1)
         return int(cpp_num)
 
-    def getContent(self):
-        return self.view.substr(sublime.Region(0, self.view.size()))
+    def getContent(self, view):
+        return view.substr(sublime.Region(0, view.size()))
 
     def fillFreopen(self, content, problem):
         result = content
@@ -98,10 +105,16 @@ class SmojSubmitCommand(sublime_plugin.TextCommand):
         result = re.sub(_cm2_re, r'\1\2\3'                                      , result)
         return result
 
-    def run(self, edit):
-        problem = self.getProblemNum()
-        if not problem:
-            return None
-        content = self.getContent()
-        content = self.fillFreopen(content, problem)
-        self.post(content, problem)
+    def run(self, **kw):
+        if kw['type'] == 'login':
+            username = self.setting.get('username')
+            password = self.setting.get('password')
+            self.relogin(username, password)
+        elif kw['type'] == 'submit':
+            view = sublime.active_window().active_view()
+            problem = self.getProblemNum(view)
+            if not problem:
+                return None
+            content = self.getContent(view)
+            content = self.fillFreopen(content, problem)
+            self.post(content, problem, view)
