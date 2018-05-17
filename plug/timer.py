20f8ac85cc2249da3cd78007e304ab83ef323148
YanWQ-monad <YanWQmonad@gmail.com>
2018-05-18 07:46:38 +0800

    Add plugin timer

---
+++ plug/timer.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from threading import Thread
+import time
+
+class TimerThread(Thread):
+	def __init__(self, delay, callback):
+		self.delay = delay
+		self.callback = callback
+		Thread.__init__(self)
+
+	def run(self):
+		time.sleep(self.delay)
+		self.callback()
+
+class Timer:
+	def __init__(self, delay, callback):
+		timer = TimerThread(delay, callback)
+		timer.start()
